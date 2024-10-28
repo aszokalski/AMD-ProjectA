cf70b077c71110b0be13fed1290f5d8bdff984df
Adam Szokalski <szokalskiadam@gmail.com>
2024-10-28 16:06:57 +0000

    Init

--- medknow/main.py
+++ medknow/main.py
@@ -5,14 +5,10 @@ app = FastAPI()
 logger = getLogger(__name__)
 
 
-# @app.on_event("startup")
-# async def startup_event():
-#     logger.info("Initializing resources...")
-
-
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
 
 @app.get("/generate_dataset")
 def generate_dataset():
