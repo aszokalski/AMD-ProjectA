08e6cdf92d9f533d09833471e612ff878d2e3ffe
Adam Szokalski <szokalskiadam@gmail.com>
2024-11-18 20:27:43 +0000

    fungidata

---
+++ fungidata/main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI, Request
+import pandas as pd
+import json
+app = FastAPI()
+
+
+@app.get("/generate_dataset")
+async def generate_dataset():
+    df = pd.read_csv("data/dataset_long_name_ORIGINAL.csv")
+    return {
+        "data": json.loads(df.to_json(orient="records"))
+    }
